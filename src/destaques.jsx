b974ce2a73123cf612b74c54fcb939a234c9cfb6
yuridaK1NG <rumosalae@gmail.com>
2022-07-04 10:35:22 -0300

    adicionando os destaques

--- src/destaques.jsx
+++ src/destaques.jsx
@@ -18,7 +18,7 @@ function Destaques(){
             </button>
 
             <button className='produto1'>
-                    <img className='imgdestaque' src='../public/big-king.webp' alt='big king'/>
+                    <img className='imgdestaque' src='../public/cheddar-duplo.webp' alt='big king'/>
                     <p className='tituloproduto'>Combo Cheddar Duplo</p>
                     <p>Um hambúrguer com duas carnes bovinas grelhadas de 57g, pão preto com gergelim......</p>
                     <p className='bold'>Serve 1 pessoa</p>
@@ -26,7 +26,7 @@ function Destaques(){
             </button>
 
             <button className='produto1'>
-                    <img className='imgdestaque' src='../public/big-king.webp' alt='big king'/>
+                    <img className='imgdestaque' src='../public/cheeseburger-duplo.webp' alt='big king'/>
                     <p className='tituloproduto'>Combo Cheeseburguer Duplo com Bacon</p>
                     <p>Um hambúrguer com duas carnes bovinas grelhadas de 57g, pão com gergelim, queijo...</p>
                     <p className='bold'>Serve 1 pessoa</p>
@@ -34,7 +34,7 @@ function Destaques(){
             </button>
 
             <button className='produto1'>
-                    <img className='imgdestaque' src='../public/big-king.webp' alt='big king'/>
+                    <img className='imgdestaque' src='../public/mega-stacker.webp' alt='big king'/>
                     <p className='tituloproduto'>Combo Mega Stacker 2.0</p>
                     <p>Um hambúrguer com duas carnes bovinas grelhadas de 113g, pão com gergelim, queijo...</p>
                     <p className='bold'>Serve 1 pessoa</p>
